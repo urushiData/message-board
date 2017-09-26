f057be85e1cbe9d4b1ed23161cb6bd06c2ef6d0e
shobun <tumaimesmyprincer@gmail.com>
2017-09-26 21:06:57 +0000

    Dont'tRepeatYourslef

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  
   def index
     @messages = Message.all
   end
 
   def show
-    @message = Message.find(params[:id])
   end
 
   def new
@@ -24,11 +25,9 @@ class MessagesController < ApplicationController
   end
 
   def edit
-    @message = Message.find(params[:id])
   end
 
   def update
-    @message = Message.find(params[:id])
     
     if @message.update(message_params)
       flash[:success] = 'Updated Successfully'
@@ -40,7 +39,6 @@ class MessagesController < ApplicationController
   end
   
   def destroy
-    @message = Message.find(params[:id])
     @message.destroy
     
     flash[:success] = 'Deleted Successfully'
@@ -48,6 +46,13 @@ class MessagesController < ApplicationController
   end
   
   private
+  
+  # 共通
+  def set_message
+    @message = Message.find(params[:id])
+  end
+      
+  
   # Strong Parameter
   def message_params
     params.require(:message).permit(:content)
